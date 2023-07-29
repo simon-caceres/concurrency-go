a4081a10cffdae30882705faea332f7b2a0be5fa
simon caceres <simon.caceres@mercadolibre.cl>
2023-07-28 21:46:39 -0400

    :hammer: stargin emails templates and email test

--- README.m
+++ README.m
@@ -1,1 +1,35 @@
 # concurrency-go
+
+## install mailhog for testing
+```
+go get github.com/mailhog/mhsendmail
+```
+## run mailhog local
+```
+mhsendmail -smtp-addr=localhost:1025
+
+or
+
+
+mailhog
+```
+
+## install redis
+```
+brew install redis
+```
+
+## run redis local
+```
+redis-server
+```
+
+## install postgresql
+```
+brew install postgresql
+```
+
+## run postgresql local
+```
+pg_ctl -D /usr/local/var/postgres start
+```
